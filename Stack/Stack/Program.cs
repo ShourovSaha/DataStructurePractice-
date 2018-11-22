5eebc6eeb74f01bcbcaff049011c606a27e14ebc
ShourovSaha <shourovsaha94@gmail.com>
2018-11-22 16:32:10 +0600

    Stack implementation in C#.

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node();
+            node = null;
+            //node.Data = 10;
+            //node.Next = null;
+
+            StackRepository stackRepository = new StackRepository();
+            Node headNode = null;
+
+            int listLength = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < listLength; i++)
+            {
+                int data = Convert.ToInt32(Console.ReadLine());
+                headNode = stackRepository.Push(headNode, data);
+            }
+
+            stackRepository.Print(headNode);
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+            Console.WriteLine("------After 1 Pop--------");
+            headNode = stackRepository.Pop(headNode);
+            stackRepository.Print(headNode);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack/Stack/StackRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackRepository
+    {
+        //private Node _node;
+
+        //public StackRepository()
+        //{
+        //    _node = new Node();
+        //}
+
+        public Node Push(Node node, int data)
+        {
+            if (node == null)
+            {
+                Node newNode = new Node();
+                newNode.Data = data;
+                newNode.Next = null;
+                return newNode;
+            }
+            else
+            {
+                Node headNode = node;
+                Node newNode = new Node();
+                newNode.Data = data;
+                newNode.Next = headNode;
+                headNode = newNode;
+                return headNode;
+            }
+        }
+
+        public Node Pop(Node node)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("No data to delete!");
+                //return node;
+            }
+            else
+            {
+                Node temp = node;
+                temp = temp.Next;
+                node = temp;
+
+            }
+
+            return node;
+        }
+
+        public void Print(Node node)
+        {
+            while (node != null)
+            {
+                Console.Write("-->" + node.Data);
+                node = node.Next;
+            }
+        }
+
+    }
+
+}
